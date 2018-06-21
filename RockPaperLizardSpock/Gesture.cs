3937a5391364eff731ef1456842425044820f346
cryptofox3 <pghodgson558@gmail.com>
2018-06-21 11:16:04 -0500

    adding classes and building out structure

--- RockPaperLizardSpock/Gesture.cs
+++ RockPaperLizardSpock/Gesture.cs
@@ -8,6 +8,12 @@ namespace RockPaperLizardSpock
 {
     class Gesture
     {
+        List<string> gestures = new List<string>();
+            gestures.Add("Rock");
+            gestures.Add("Paper");
+            gestures.Add("Scissors");
+            gestures.Add("Lizard");
+            gestures.Add("Spock");
 
     }
 }
